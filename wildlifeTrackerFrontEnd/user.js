03e8867db2d3430b9c2da9c2158243f99133c729
github username <github email address>
2019-11-04 14:06:20 -0700

    took out google maps, the api key no longer works and there was no functionality for the map

--- wildlifeTrackerFrontEnd/user.js
+++ wildlifeTrackerFrontEnd/user.js
@@ -25,39 +25,39 @@
 // })
 
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('googleMap'),{
-    center: {lat: 39.766636, lng: -104.980210}, // lat/long of center of map
-    zoom: 8, // 8 or 9 is typical zoom 
-    scrollwheel:  false, // prevent mouse scroll from zooming map. 
-    mapTypeControl: true, //default
-    mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-        position: google.maps.ControlPosition.BOTTOM_CENTER
-    },
-    zoomControl: true, //default
-    zoomControlOptions: {
-        position: google.maps.ControlPosition.LEFT_CENTER
-    },
-    streetViewControl: true, //default
-    streetViewControlOptions: {
-        position: google.maps.ControlPosition.LEFT_TOP
-    }, 
-    fullscreenControl: true
-  });
-var marker
-  map.addListener('click', function(e){
-      placeMarker(e.latLng, map);
-  })
-  function placeMarker(position, map) {
-    var marker = new google.maps.Marker({
-        position: position,
-        map: map
-    });
-    map.panTo(position);
-  }
-}
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('googleMap'),{
+//     center: {lat: 39.766636, lng: -104.980210}, // lat/long of center of map
+//     zoom: 8, // 8 or 9 is typical zoom 
+//     scrollwheel:  false, // prevent mouse scroll from zooming map. 
+//     mapTypeControl: true, //default
+//     mapTypeControlOptions: {
+//         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+//         position: google.maps.ControlPosition.BOTTOM_CENTER
+//     },
+//     zoomControl: true, //default
+//     zoomControlOptions: {
+//         position: google.maps.ControlPosition.LEFT_CENTER
+//     },
+//     streetViewControl: true, //default
+//     streetViewControlOptions: {
+//         position: google.maps.ControlPosition.LEFT_TOP
+//     }, 
+//     fullscreenControl: true
+//   });
+// var marker
+//   map.addListener('click', function(e){
+//       placeMarker(e.latLng, map);
+//   })
+//   function placeMarker(position, map) {
+//     var marker = new google.maps.Marker({
+//         position: position,
+//         map: map
+//     });
+//     map.panTo(position);
+//   }
+// }
 
 
 
